361218aad7ba1b537bb47ce6906e6aa51fb5033e
d.popov <dmitrypopof@mail.ru>
2026-03-31 23:07:41 +0300

    add click button search

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchBut = findViewById<Button>(R.id.search_button)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку поиска!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchBut.setOnClickListener (buttonClickListener)
     }
 }
