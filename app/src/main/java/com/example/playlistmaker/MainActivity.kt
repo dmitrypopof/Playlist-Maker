9e69e4bf342efe33d894c2b90464a8b3ad9f211b
d.popov <dmitrypopof@mail.ru>
2026-04-05 22:20:55 +0300

    add enableEdgeToEdge() in MainActivity.kt

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -4,29 +4,36 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(top = systemBars.top)
+            insets
+        }
 
         val searchBut = findViewById<Button>(R.id.search_button)
         val mediaBut = findViewById<Button>(R.id.media_button)
         val settingBut = findViewById<Button>(R.id.setting_button)
 
 
-
-
-
-        val buttonClickListener: View.OnClickListener = object : View.OnClickListener{
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
                 //Toast.makeText(this@MainActivity, "Нажали на кнопку поиска!", Toast.LENGTH_SHORT).show()
                 val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
                 startActivity(searchIntent)
             }
         }
-        searchBut.setOnClickListener (buttonClickListener)
+        searchBut.setOnClickListener(buttonClickListener)
 
 
         mediaBut.setOnClickListener {
@@ -41,8 +48,6 @@ class MainActivity : AppCompatActivity() {
             val setIntent = Intent(this, SettingsActivity::class.java)
             startActivity(setIntent)
         }
-
-
 
 
     }
