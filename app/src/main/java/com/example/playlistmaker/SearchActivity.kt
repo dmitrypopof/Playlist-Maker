34bf95543c934b35991ce30b5c37b5ee74376618
d.popov <dmitrypopof@mail.ru>
2026-04-26 16:14:09 +0300

    EditText activity_search v1.1 -- исправление верстки. логика удаления ввода

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,14 +3,14 @@ package com.example.playlistmaker
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +24,41 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val backButton = findViewById<MaterialButton>(R.id.back_button)
-        val searchField = findViewById<AppCompatEditText>(R.id.search_field)
+        val inputEditText = findViewById<AppCompatEditText>(R.id.search_field2)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
         backButton.setOnClickListener {
             finish()
         }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
 
+        val simpleTextWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
 
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
 
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+        clearButton.visibility = clearButtonVisibility(inputEditText.text)
+        //inputEditText.requestFocus()
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
