c8c40048a413b4058ad90b16dcaa98cb1ce6efef
d.popov <dmitrypopof@mail.ru>
2026-04-26 13:00:53 +0300

    EditText activity_search v1.1 -- исправление верстки. завершил

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,16 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +24,13 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val backButton = findViewById<MaterialButton>(R.id.back_button)
+        val searchField = findViewById<AppCompatEditText>(R.id.search_field)
 
         backButton.setOnClickListener {
             finish()
         }
+
+
+
     }
 }
