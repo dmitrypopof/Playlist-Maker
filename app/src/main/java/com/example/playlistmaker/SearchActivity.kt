961f3e9f16cea6d16f5bf4279c7c82751ec9f87e
d.popov <dmitrypopof@mail.ru>
2026-04-26 18:07:30 +0300

    Хранение данных v.1

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -13,6 +13,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
+
+    private var bufferValue: String = TEXT_DEF
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,7 +27,8 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val backButton = findViewById<MaterialButton>(R.id.back_button)
-        val inputEditText = findViewById<AppCompatEditText>(R.id.search_field2)
+        val inputEditText = findViewById<AppCompatEditText>(R.id.search_field)
+        inputEditText.setText(bufferValue)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
         backButton.setOnClickListener {
@@ -32,6 +36,7 @@ class SearchActivity : AppCompatActivity() {
         }
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            bufferValue = ""
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -44,6 +49,7 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                bufferValue = s?.toString() ?: ""
                 clearButton.visibility = clearButtonVisibility(s)
             }
 
@@ -53,6 +59,15 @@ class SearchActivity : AppCompatActivity() {
         clearButton.visibility = clearButtonVisibility(inputEditText.text)
         //inputEditText.requestFocus()
     }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INPUT_TEXT, bufferValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        bufferValue = savedInstanceState.getString(INPUT_TEXT, TEXT_DEF)
+    }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
@@ -60,5 +75,10 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    companion object{
+        const val INPUT_TEXT = "INPUT_TEXT"
+        const val TEXT_DEF = ""
     }
 }
