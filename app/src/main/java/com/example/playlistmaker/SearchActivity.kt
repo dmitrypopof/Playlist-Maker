6c00d7d201a7bd3b0a5c3d37a20221e2e4c22042
d.popov <dmitrypopof@mail.ru>
2026-05-11 19:39:02 +0300

    добавил adapter и стили - завершил

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -13,7 +13,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.playlistmaker.adapters.TrackAdapter
 import com.google.android.material.button.MaterialButton
 
