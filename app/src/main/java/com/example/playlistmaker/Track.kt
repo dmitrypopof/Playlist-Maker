d90d7232258907bc4886598e54ed031064572ae0
d.popov <dmitrypopof@mail.ru>
2026-05-15 22:25:41 +0300

    добавил ключи в Track.kt аннотацией SerializedName

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,10 +1,11 @@
 package com.example.playlistmaker
 
-data class Track(
-    val trackName: String,
-    val artistName: String,
-    val trackTime: String,
-    val artworkUrl100: String
-) {
+import com.google.gson.annotations.SerializedName
 
+data class Track(
+@SerializedName("trackName") val trackName: String,
+@SerializedName("artistName") val artistName: String,
+@SerializedName("trackTimeMillis") val trackTime: String,
+@SerializedName("artworkUrl100") val artworkUrl100: String
+) {
 }
