ba8e83c231de9cccb65e2aaf93544b0210b6175e
d.popov <dmitrypopof@mail.ru>
2026-05-16 10:44:36 +0300

    форматирование миллисек в Track.kt

--- app/src/main/java/com/example/playlistmaker/models/Track.kt
+++ app/src/main/java/com/example/playlistmaker/models/Track.kt
@@ -1,10 +1,17 @@
 package com.example.playlistmaker.models
 
 import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 data class Track(
-    @SerializedName("trackName") val trackName: String,
-    @SerializedName("artistName") val artistName: String,
-    @SerializedName("trackTimeMillis") val trackTime: String,
-    @SerializedName("artworkUrl100") val artworkUrl100: String
-)
+    @SerializedName("trackName") val trackName: String?,
+    @SerializedName("artistName") val artistName: String?,
+    @SerializedName("trackTimeMillis") val trackTime: Long?,
+    @SerializedName("artworkUrl100") val artworkUrl100: String?
+) {
+    val formattedTime: String
+        get() = trackTime?.let { millis ->
+            SimpleDateFormat("mm:ss", Locale.getDefault()).format(millis)
+        } ?: "00:00"
+}
