a15d81c0ded292c35c37aeb03ebb2989f06d7ee6
d.popov <dmitrypopof@mail.ru>
2026-05-17 12:57:54 +0300

    Этап 2: Отправка запроса

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
@@ -14,12 +15,19 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playlistmaker.adapters.TrackAdapter
+import com.example.playlistmaker.helpers.RetrofitHelper
 import com.example.playlistmaker.models.Track
+import com.example.playlistmaker.models.TrackResponse
 import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
 
     private var bufferValue: String = TEXT_DEF
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var placeholderSearch: LinearLayout
+    private lateinit var stubNoResult: LinearLayout
+    private lateinit var adapter: TrackAdapter
+    private lateinit var inputEditText: AppCompatEditText
 
     // В начало класса SearchActivity, после объявления bufferValue
     private val searchRunnable = Runnable {
@@ -39,8 +47,11 @@ class SearchActivity : AppCompatActivity() {
         }
 
         val backButton = findViewById<MaterialButton>(R.id.back_button)
-        val inputEditText = findViewById<AppCompatEditText>(R.id.search_field)
-        inputEditText.setText(bufferValue)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        placeholderSearch = findViewById(R.id.placeholder_search)
+        stubNoResult = findViewById(R.id.stub_no_result)
+        inputEditText = findViewById(R.id.search_field)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
         backButton.setOnClickListener {
@@ -83,42 +94,14 @@ class SearchActivity : AppCompatActivity() {
         clearButton.visibility = clearButtonVisibility(inputEditText.text)
         //inputEditText.requestFocus()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val tracks = listOf(
-            Track(
-                trackName = "Smells Like Teen Spirit",
-                artistName = "Nirvana",
-                trackTime = "5:01",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Billie Jean",
-                artistName = "Michael Jackson",
-                trackTime = "4:35",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Stayin' Alive",
-                artistName = "Bee Gees",
-                trackTime = "4:10",
-                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Whole Lotta Love",
-                artistName = "Led Zeppelin",
-                trackTime = "5:33",
-                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            ),
-            Track(
-                trackName = "Sweet Child O'Mine",
-                artistName = "Guns N' Roses",
-                trackTime = "5:03",
-                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        val adapter = TrackAdapter(tracks)
+        adapter = TrackAdapter(emptyList())
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
+
+        // При старте скрываем все (пустой экран)
+        recyclerView.visibility = View.GONE
+        placeholderSearch.visibility = View.GONE
+        stubNoResult.visibility = View.GONE
     }
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -145,6 +128,29 @@ class SearchActivity : AppCompatActivity() {
 
     private fun performSearch(query: String) {
         android.util.Log.d("SearchActivity", "Поисковой запрос: $query")
-        // Здесь будет логика отправки запроса (Этап 2)
+        // Показываем RecyclerView, скрываем заглушки
+        recyclerView.visibility = View.VISIBLE
+        placeholderSearch.visibility = View.GONE
+        stubNoResult.visibility = View.GONE
+
+        // Выполняем запрос
+        val call = RetrofitHelper.apiService.search(query)
+        call.enqueue(object : retrofit2.Callback<TrackResponse> {
+            override fun onResponse(
+                call: retrofit2.Call<TrackResponse>,
+                response: retrofit2.Response<TrackResponse>
+            ) {
+                // Обработка ответа будет на Этапе 3
+                android.util.Log.d("SearchActivity", "Ответ получен. Код: ${response.code()}")
+            }
+
+            override fun onFailure(
+                call: retrofit2.Call<TrackResponse>,
+                t: Throwable
+            ) {
+                // Обработка ошибки будет на Этапе 3
+                android.util.Log.e("SearchActivity", "Ошибка сети: ${t.message}")
+            }
+        })
     }
 }
