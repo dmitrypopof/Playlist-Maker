029b2c3733e61db3a215242d7903ca0d933e6642
d.popov <dmitrypopof@mail.ru>
2026-08-02 21:37:29 +0300

    SettingsActivity.kt перевел на ViewBinding

--- app/src/main/java/com/example/playlistmaker/presentation/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/ui/settings/SettingsActivity.kt
@@ -10,11 +10,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.playlistmaker.Creator
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivitySettingsBinding
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
 
     private val getThemeSettingsUseCase by lazy { Creator.provideGetThemeSettingsUseCase() }
     private val updateThemeSettingsUseCase by lazy { Creator.provideUpdateThemeSettingsUseCase() }
@@ -22,10 +25,10 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val themeSwitcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
-        themeSwitcher.isChecked = getThemeSettingsUseCase()
+        binding.themeSwitcher.isChecked = getThemeSettingsUseCase()
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -33,17 +36,11 @@ class SettingsActivity : AppCompatActivity() {
             insets
         }
 
-        val backButton = findViewById<MaterialButton>(R.id.back_button)
-        val shareButton = findViewById<MaterialTextView>(R.id.shareApp)
-        val supButton = findViewById<MaterialTextView>(R.id.writeSupport)
-        val agreementButton = findViewById<MaterialTextView>(R.id.userAgreement)
-
-
-        backButton.setOnClickListener {
+        binding.backButton.setOnClickListener {
             finish()
         }
 
-        themeSwitcher.setOnCheckedChangeListener { _, checked ->
+        binding.themeSwitcher.setOnCheckedChangeListener { _, checked ->
             updateThemeSettingsUseCase(checked)
             AppCompatDelegate.setDefaultNightMode(
                 if (checked) {
@@ -54,7 +51,7 @@ class SettingsActivity : AppCompatActivity() {
             )
         }
 
-        shareButton.setOnClickListener {
+        binding.shareApp.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
             intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.link_androidDeveloper))
@@ -66,7 +63,7 @@ class SettingsActivity : AppCompatActivity() {
             )
         }
 
-        supButton.setOnClickListener {
+        binding.writeSupport.setOnClickListener {
             val intent = Intent(Intent.ACTION_SENDTO)
             intent.data = "mailto:".toUri()
             intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.sendMail)))
@@ -82,7 +79,7 @@ class SettingsActivity : AppCompatActivity() {
 
         }
 
-        agreementButton.setOnClickListener {
+        binding.userAgreement.setOnClickListener {
             val intent =
                 Intent(Intent.ACTION_VIEW, getString(R.string.linkPracticumOffer).toUri())
             startActivity(
