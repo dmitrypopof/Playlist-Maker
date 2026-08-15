84fe093fa6b3dc26161ca8457cd6144781208fc3
d.popov <dmitrypopof@mail.ru>
2026-08-15 16:12:21 +0300

    создал SearchState.kt

---
+++ app/src/main/java/com/example/playlistmaker/feature/search/presentation/SearchState.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker.feature.search.presentation
+
+import com.example.playlistmaker.feature.search.domain.model.Track
+
+sealed class SearchState {
+    object Loading : SearchState()
+    object Empty : SearchState()
+    object HistoryEmpty : SearchState()
+    object NetworkError : SearchState()
+
+    data class Content(
+        val tracks: List<Track>
+    ) : SearchState()
+
+    data class HistoryContent(
+        val tracks: List<Track>
+    ) : SearchState()
+}
