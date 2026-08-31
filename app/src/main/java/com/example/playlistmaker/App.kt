6c7931d8cadbe8524c337dbada4643546ee4a092
d.popov <dmitrypopof@mail.ru>
2026-08-31 21:12:44 +0300

    Koin в App.kt

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -2,6 +2,12 @@ package com.example.playlistmaker
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.playlistmaker.di.dataModule
+import com.example.playlistmaker.di.interactorModule
+import com.example.playlistmaker.di.repositoryModule
+import com.example.playlistmaker.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 
 class App: Application() {
@@ -15,6 +21,12 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         Creator.init(this)
+
+        startKoin {
+            androidContext(this@App)
+            modules(dataModule, repositoryModule, interactorModule, viewModelModule)
+        }
+
         val prefs = getSharedPreferences("app_settings", MODE_PRIVATE)
         darkTheme = prefs.getBoolean(THEME_PREF_KEY, false)
 
