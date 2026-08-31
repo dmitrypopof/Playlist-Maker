9b45b966190eca5d0acab371212de406fce4ca13
d.popov <dmitrypopof@mail.ru>
2026-08-31 21:05:13 +0300

    создание viewModelModule

---
+++ app/src/main/java/com/example/playlistmaker/di/viewModelModule.kt
@@ -0,0 +1,27 @@
+package com.example.playlistmaker.di
+
+import com.example.playlistmaker.feature.main.presentation.MainViewModel
+import com.example.playlistmaker.feature.player.presentation.AudioPlayerViewModel
+import com.example.playlistmaker.feature.search.presentation.SearchViewModel
+import com.example.playlistmaker.feature.settings.presentation.SettingsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+    viewModel {
+        MainViewModel()
+    }
+
+    viewModel {
+        SearchViewModel(get(), get(), get(), get())
+    }
+
+    viewModel {
+        AudioPlayerViewModel(get())
+    }
+
+    viewModel {
+        SettingsViewModel(get(), get())
+    }
+}
