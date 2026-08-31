ba78f0897f5e39b783a8986a2d1b29dac592caf4
d.popov <dmitrypopof@mail.ru>
2026-08-31 14:12:09 +0300

    пакет di и модуль для слоя Data

---
+++ app/src/main/java/com/example/playlistmaker/di/dataModule.kt
@@ -0,0 +1,32 @@
+package com.example.playlistmaker.di
+
+import android.content.Context
+import com.example.playlistmaker.feature.search.data.network.ITunesApi
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val dataModule = module {
+
+    single<ITunesApi> {
+        Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ITunesApi::class.java)
+    }
+
+    single {
+        androidContext()
+            .getSharedPreferences("history_prefs", Context.MODE_PRIVATE)
+    }
+
+    single {
+        androidContext()
+            .getSharedPreferences("app_settings", Context.MODE_PRIVATE)
+    }
+
+    factory { Gson() }
+}
