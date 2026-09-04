4e38a5c92edd9431e36adf451d054925fd4f4887
d.popov <dmitrypopof@mail.ru>
2026-09-04 09:41:21 +0300

    фикс MediaActivity.kt, xml

--- app/src/main/java/com/example/playlistmaker/feature/media/presentation/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/feature/media/presentation/MediaActivity.kt
@@ -5,20 +5,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMediaBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMediaBinding
+    private lateinit var tabMediator: TabLayoutMediator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMediaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupBackButton()
+        setupViewPager()
+    }
+
+    private fun setupBackButton() {
+        binding.backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun setupViewPager() {
+        binding.viewPager.adapter = MediaPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                MediaPagerAdapter.FAVORITE_TRACKS_POSITION -> getString(R.string.tab_favorite_tracks)
+                MediaPagerAdapter.PLAYLISTS_POSITION -> getString(R.string.tab_playlists)
+                else -> ""
+            }
+        }
+        tabMediator.attach()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 }
